8237c4a273339134585dc17b0944b73b03a48603
santiago.carullo@gmail.com <santiago.carullo@gmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-09-13 21:53:03 +0000

    Agregado a las funciones leer_sector() y escribir_sector() los avisos posix_fadvise() y posix_madvise() a ppd_io_map.c y ppd_io_stream.c respectivamente, no se si esta bien el tipo de aviso

--- PPD/src/ppd_io_stream.c
+++ PPD/src/ppd_io_stream.c
@@ -9,6 +9,11 @@ int32_t leer_sector(int32_t sector, char* buf)
 {
 	bytes_perSector = 512; //bytes_perSector tendria que ser variable global al PPD ya que es información importante
 	uint32_t file_descriptor = open("/home/utn_so/FUSELAGE/fat32.disk",O_RDONLY);
+	/*
+	 * Aviso al SO sobre el uso del archivo ?
+	 */
+	posix_fadvise(file_descriptor,bytes_perSector*sector,bytes_perSector,POSIX_FADV_SEQUENTIAL);
+
 	if (lseek(file_descriptor,bytes_perSector*sector,SEEK_SET) == 0) //Posiciono el cursor dentro del archivo al inicio del sector buscado
 	{
 		if (read(file_descriptor,buf,bytes_perSector) == bytes_perSector) //Copio a buf el sector buscado
@@ -28,6 +33,12 @@ int32_t escribir_sector(int32_t sector, char *buf)
 {
 	bytes_perSector = 512; //bytes_perSector tendria que ser variable global al PPD ya que es información importante
 	uint32_t file_descriptor = open("/home/utn_so/FUSELAGE/fat32.disk",O_RDWR);
+
+	/*
+	* Aviso al SO sobre el uso del archivo ?
+	*/
+	posix_fadvise(file_descriptor,bytes_perSector*sector,bytes_perSector,POSIX_FADV_SEQUENTIAL);
+
 	if (lseek(file_descriptor,bytes_perSector*sector,SEEK_SET) == 0) //Posiciono el cursor dentro del archivo al inicio del sector buscado
 	{
 		if (write(file_descriptor,buf,bytes_perSector) == bytes_perSector) //Copio en el archivo el contenido de buf o sea el sector
