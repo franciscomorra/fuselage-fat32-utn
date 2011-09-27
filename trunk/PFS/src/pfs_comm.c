d8f47b1988343f59c384d2377944fd1147587d90
mauser_fdm@hotmail.com <mauser_fdm@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-09-27 02:54:55 +0000

    cambiando a msgNIPC_t, hay que corregir el linkeo con COMMONS y el msgNIPC_t msg.len que esta en char[2] (ver como se trabaja con la unidad)

--- trunk/PFS/src/pfs_comm.c
+++ trunk/PFS/src/pfs_comm.c
@@ -5,64 +5,59 @@
  *      Author: utn_so
  */
 
+//PROBLEMAS A SOLUCIONAR: LINKEO CON COMMONS Y PASAJE DE LENGTH DE NIPC
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include "pfs_comm.h"
 #include "ppd_io.h"
+#include "nipc.h"
+
 //ACA SE HACE LA CONEXION POR SOCKET Y LA VARIABLE QUE LA REPRESENTE SERA static
 //PARA QUE SU SCOPE SEA SOLO DENTRO DE ESTE ARCHIVO QUE MANEJARA LAS CONEXIONES
 
-char* PFS_requestSectorsRead(uint32_t *sectors,size_t sectors_count)
+msgNIPC_t PFS_requestSectorsRead(uint32_t *sectors,size_t sectors_count)
 {
-	char type = READ;
+	msgNIPC_t msg;
+	msg.type = READ;
 	uint32_t index;
-	size_t len = sectors_count*sizeof(uint32_t);
-	uint32_t* payload = malloc(sectors_count*sizeof(uint32_t));
 
+	msg.len = sectors_count*sizeof(uint32_t);//VER COMO SE PASA DE CHAR[2] A SIZE_T
+
+	msg.payload = malloc(sectors_count*sizeof(uint32_t));
 	for (index=0;index<sectors_count;index++)
 	{
-		memcpy(payload+index,sectors+index,sizeof(uint32_t));
-
+		memcpy(msg.payload+index,sectors+index,sizeof(uint32_t));
 	}
 
-	char* msg = malloc(1+2+len);
-	memcpy(msg,&type,1);
-	memcpy(msg+1,&len,2);
-	memcpy(msg+3,payload,len);
-	char *buf = PFS_request(msg);
-	return buf;
+	return msg;
+
 }
 
-uint32_t PFS_requestSectorsWrite(uint32_t *sectors)
+msgNIPC_t PFS_requestSectorsWrite(uint32_t *sectors)
 {
 
-
 }
 
-char* PFS_request(char* msg)
+char* PFS_request(msgNIPC_t msg)
 {
 	//CAMBIAR POR ENVIO POR SOCKET
 	//CAMBIAR POR ENVIO POR SOCKET
 	//CAMBIAR POR ENVIO POR SOCKET
 
-	//Descomponemos el mensaje en sus partes
-	uint32_t len;
-	char type = *msg;
-	memcpy(&len,msg+1,2);
-	uint32_t* payload = malloc(len);
-	memcpy(payload,msg+3,len);
-	//-----
-
-	if (type == READ)
+	if (msg.type == READ)
 	{
 		uint32_t file_descriptor = open("/home/utn_so/FUSELAGE/fat32.disk",O_RDWR); //TEMPORAL
-		size_t count = len/sizeof(uint32_t);
+
+		//size_t count = len/sizeof(uint32_t);
+		size_t count = msg.len;//VER COMO SE PASA DE CHAR[2] A SIZE_T
+
 		uint32_t i;
 		char *buf = malloc(512*count);
 		for (i=0;i<count;i++)
 		{
-			read_sector(file_descriptor,payload[i],buf+(512*i));
+			read_sector(file_descriptor,msg.payload[i],buf+(512*i));
 		}
 		close(file_descriptor);
 		return buf;
