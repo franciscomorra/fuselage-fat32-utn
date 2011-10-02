0b57c69aabcab422b7ab3a07781908af742a05ae
santiago.carullo@gmail.com <santiago.carullo@gmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-02 23:44:21 +0000

    - No hagamos commit de las compilaciones gente

--- trunk/PPD/Debug_PPD/src/ppd_requestList.d
+++
@@ -1,4 +0,0 @@
-src/ppd_requestList.d src/ppd_requestList.o: ../src/ppd_requestList.c \
- /home/utn_so/Desktop/trabajos/PPD/include/ppd_requestList.h
-
-/home/utn_so/Desktop/trabajos/PPD/include/ppd_requestList.h:
