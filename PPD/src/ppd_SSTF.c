474fecc6b664f08a3d676bd1558d834847e48700
nico_imf@hotmail.com <nico_imf@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-03 20:10:54 +0000

    Probamos el SSTF y además cambiamos unos values del archivo de configuración del PPD Como el archivo es de 512 mb hicimos:
    * total de sectores = 1048576 (512 MB / 512 B)
    * total de cilindros = 65536 (1048576 / 16)
    * sectores por cilindro = 16 para que de un numero entero
    * cabezas = 1 xq lo dice el tp
    Entonces el CHS = (65536,1,16)
    * RPM = 3750 para q el tiempo q tarde en pasar sobre un sector sea de 1ms (para que sea mas comodo)
    
    Por último cambiamos un par de nombres de algunas funciones relativas al SSTF y del .c y el .h

--- PPD/src/ppd_SSTF.c
+++ PPD/src/ppd_SSTF.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include "ppd_requestList.h"
+#include "ppd_SSTF.h"
 
 extern uint32_t Head;
 extern uint32_t Sector;
 extern requestNode_t* first;
 
-uint32_t REQUEST_add(uint32_t* sectorNum){
+uint32_t SSTF_addRequest(uint32_t* sectorNum){
 
-	 requestNode_t* new = REQUEST_turnToCHS(sectorNum);
+	 requestNode_t* new = SSTF_turnToCHS(sectorNum);
 
 	 if(first == 0){
 		 first = new;
@@ -21,7 +21,7 @@ uint32_t REQUEST_add(uint32_t* sectorNum){
 		requestNode_t* aux = first;
 
 		while (aux->next != 0){
-			if(REQUEST_near(new,aux,aux->next)){
+			if(SSTF_near(new,aux,aux->next)){
 				new->next = aux->next;
 				aux->next = new;
 				return 0;
@@ -34,18 +34,18 @@ uint32_t REQUEST_add(uint32_t* sectorNum){
 	 return 0;
 }
 
-requestNode_t* REQUEST_turnToCHS(uint32_t* sectorNum){
+requestNode_t* SSTF_turnToCHS(uint32_t* sectorNum){
 
 	requestNode_t* new = malloc(sizeof(requestNode_t));
 
-	new->cylinder = *sectorNum / (Sector * Head);
+	new->cylinder = (*sectorNum) / (Sector * Head);
 	new->head = (*sectorNum % (Sector * Head)) / Sector;
 	new->sector = (*sectorNum % (Sector * Head)) % Sector;
 
 	return new;
 }
 
-uint32_t REQUEST_near(requestNode_t* new, requestNode_t* aux,requestNode_t* auxSig){
+uint32_t SSTF_near(requestNode_t* new, requestNode_t* aux,requestNode_t* auxSig){
 
 	//se fija si la distancia entre  new y aux es menor que la de aux y auxSig
 	// si es asi devuelve True
@@ -67,13 +67,13 @@ uint32_t REQUEST_near(requestNode_t* new, requestNode_t* aux,requestNode_t* auxS
 		return 1;
 	else
 		if (distSectorNA == distSectorAS){
-			if(REQUEST_sectorDist((aux->sector)+1,new->sector) <= REQUEST_sectorDist((aux->sector)+1,auxSig->sector))
+			if(SSTF_sectorDist((aux->sector)+1,new->sector) <= SSTF_sectorDist((aux->sector)+1,auxSig->sector))
 				return 1;
 	}
 	return 0;
 }
 
-uint32_t REQUEST_sectorDist(uint32_t fstSector, uint32_t lstSector){
+uint32_t SSTF_sectorDist(uint32_t fstSector, uint32_t lstSector){
 
 	//devuelve la cantidad de sectores que tiene que recorrer para llegar de fst a lst.
 
