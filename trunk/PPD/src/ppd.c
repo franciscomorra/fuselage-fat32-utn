0cc73930d29ad4459bff944df645389113b26fba
nico_imf@hotmail.com <nico_imf@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-04 01:14:57 +0000

    Agregamos la configuración del tiempo de salto de pistas y los cambios q se necesitan para tenerlo en cuenta dentro del algoritmo SSTF.

--- trunk/PPD/src/ppd.c
+++ trunk/PPD/src/ppd.c
@@ -7,23 +7,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "config_manager.h"
 #include "ppd_SSTF.h"
 
 uint32_t Cylinder;
 uint32_t Head;
 uint32_t Sector;
+uint32_t TrackJumpTime;
 requestNode_t* first;
 
 int main(int argc, char *argv[])
 {
 	first = 0;
+
 /*	int i;
- *	uint32_t vec[7] = {512,528, 802, 544, 816, 1526, 483};
+ *	uint32_t vec[7] = {512,534, 802, 498, 816, 1526, 483};
  *	uint32_t* p = malloc(7*sizeof(uint32_t));
  *
- * 	memcpy(p,vec,7*4);
- * 							sirve para probar la lista del algoritmo sstf
+ *	memcpy(p,vec,7*4);
+ * 								sirve para probar la lista del algoritmo sstf
  */
 	config_param *ppd_config;
 	CONFIG_read("config/ppd.config",&ppd_config);
@@ -31,10 +34,11 @@ int main(int argc, char *argv[])
 	Cylinder   = atoi(CONFIG_getValue(ppd_config,"Cylinder"));
 	Head =  atoi(CONFIG_getValue(ppd_config,"Head"));
 	Sector =  atoi(CONFIG_getValue(ppd_config,"Sector"));
+	TrackJumpTime = atoi(CONFIG_getValue(ppd_config,"TrackJumpTime"));
 
 /*	for(i = 0; i < 7; i++)
  *		SSTF_addRequest(p+i);
- * idem
+ *								idem
  */
 
 	return 1;
