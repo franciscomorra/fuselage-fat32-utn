0118e3d793ef5232df21671c2b4125c613439e47
mauser_fdm@hotmail.com <mauser_fdm@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-09 17:54:14 +0000

    Comit del src de RAID, con los includes correspondientes, ver el doc de google para referencias

--- trunk/PRAID/src/praid_comm.c
+++ trunk/PRAID/src/praid_comm.c
@@ -1,10 +1,93 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <fcntl.h>
+/*
+ * praid_comm.c
+ *
+ *  Created on: 26/09/2011
+ *      Author: utn_so
+ */
+
 #include "nipc.h"
 #include "praid_comm.h"
 
+//Decodificacion del NIPC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * DEPRECADO
+ *
+uint32_t PRAID_manage_PPD(nipcMsg_t msgIn, PPDnode* first, PPDnode* last, diskInfo* sender, pfsInfo receiver){
+
+	switch (msgIn.type)	{
+
+		case HANDSHAKE:
+			if(first == last){
+				//PRAID_requestDiskInfo(sender, 0x13);
+				//PEDIR INFORMACION DEL DISCO
+			}else{
+				//ESPEJAR
+				//ESPEJAR
+				//THREAD NUEVO AL QUE LE PASO LA FUNCION, EL NODO QUE VA A HACER DE MASTER, Y EL SOCKET AL DISCO NUEVO
+			}
+		break;
+		case READ_SECTORS://Se leyo el sector ok
+			if(first == last){ //Si proviene del primer disco conectado
+				//ME SIRVE EL SECTOR 0X13??
+				//SI NO, PEDI EL 0X20
+				//SI ME SIRVE, CREA UN THREAD PARA EL DISCO
+
+			}else{
+				//SE LO MANDO AL THREAD PFS
+			}
+		break;
+		case WRITE_SECTORS:
+			//Se escribio el sector, ver si todos ya escribieron.
+			//RECORRER LA COLA DE PPDS, Y VER EL STATUS DE CADA UNO.
+			//SI ESTAN TODOS, SE LO MANDO AL PFS, SETEO PRAID_STATUS EN ACTIVE
+		break;
+	}
+	return 0;
+}
+
+
+uint32_t PRAID_manage_PFS(nipcMsg_t msgIn, PPDnode first, PPDnode last){
+diskInfo readNext;
+	switch (msgIn.type)
+	{
+
+		case HANDSHAKE:
+			//if (PRAID_STATUS!=INACTIVE){
+
+
+		break;
+
+		case WRITE_SECTORS:
+			//Enviar pedido a todos los discos
+			//SETEAR VARIABLE PRAID_STATUS EN WAIT_WRITE
+			//PRAID_requestPPD_Write(msgIn, first, last);
+		break;
+
+		case READ_SECTORS: //ELEGIR EL DISCO, MANDARSELO AL THREAD ASOCIADO
+			//readNext = PRAID_select_free_PPD(first, last);
+			//PRAID_requestPPD(msgIn, readNext);
+		break;
+	}
+	return 0;
+}
 uint32_t PRAID_takeRequest(msgNIPC_t msg,nipc_node* first, nipc_node* last)
 {
 	nipc_node* new = malloc(sizeof(nipc_node));
@@ -43,4 +126,6 @@ uint32_t PRAID_manageRequest(msgNIPC_t msg, nipc_node* first) {
 	free(aux);
 	return 0;
 }
+FIN DEPRECADO
 
+*/
---
+++ trunk/PRAID/src/praid_console.c
@@ -0,0 +1,21 @@
+/*
+ * praid_console.c
+ *
+ *  Created on: 09/10/2011
+ *      Author: utn_so
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "praid_console.h"
+
+uint32_t print_Console (char* message){
+
+	printf("%s \n",message);
+	return 0;
+}
+
