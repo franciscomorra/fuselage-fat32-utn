56bfc0ca2f29e8035d88521111da1345e12472ca
christ_2050@hotmail.com <christ_2050@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-21 21:48:06 +0000

    cambiamos la forma de obtener parametros. Falta ver el free del data en QUEUE_cleanQueue() y validar que los datos ingresados como parametros sean de tipo numerico

--- PPD_Console/include/ppdConsole_input.h
+++ PPD_Console/include/ppdConsole_input.h
@@ -12,6 +12,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tad_queue.h"
 
 /*
 uint32_t getCommand(char* input, char* command);
@@ -21,6 +22,6 @@ uint32_t getCleanSectors(char* input,uint32_t* fstSector,uint32_t* lstSector);		
 uint32_t getTraceSectors(char* input,uint32_t* traceSectors);
 */
 
-void CONSOLE_getCommand(char* input,char* command,uint32_t* parameters,uint32_t* paramLen);
+void CONSOLE_getCommand(char* input,char* command,queue_t* parameters,uint32_t* paramLen);
 
 #endif /* PPDCONSOLE_INPUT_H_ */
