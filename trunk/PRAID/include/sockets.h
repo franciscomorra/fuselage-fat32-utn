0922b4b4eaa2f1e868419907d46dc42a4d99819c
cisnerosgd@hotmail.com <cisnerosgd@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-10-28 23:36:25 +0000

    agregado sockets y el select

---
+++ trunk/PRAID/include/sockets.h
@@ -0,0 +1,13 @@
+/*
+ * sockets.h
+ *
+ *  Created on: 28/10/2011
+ *      Author: utn_so
+ */
+
+#ifndef SOCKETS_H_
+#define SOCKETS_H_
+
+uint32_t Create_Sockets_INET(uint32_t*);
+
+#endif /* SOCKETS_H_ */
