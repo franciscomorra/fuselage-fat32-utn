f8188521416f84d3a8387f68996e9b56d8cae0d9
christ_2050@hotmail.com <christ_2050@hotmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-11-15 18:13:37 +0000

    Se traslado el COMMAND_showTrace al COMMONS del log, para que tambien lo pueda utilizar el PPD y asi imprimir el archivo log

--- trunk/PPD_Console/include/ppdConsole_Command.h
+++ trunk/PPD_Console/include/ppdConsole_Command.h
@@ -10,12 +10,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
-
-typedef struct CHS_t {
-	uint32_t cylinder;
-	uint32_t head;
-	uint32_t sector;
-}__attribute__((__packed__)) CHS_t;
+#include "log.h"
 
 uint32_t console_info(uint32_t);
 
@@ -23,8 +18,8 @@ uint32_t console_clean(queue_t,uint32_t);
 
 uint32_t console_trace(queue_t,uint32_t,uint32_t);
 
-void console_turnToCHS(uint32_t* sectorNum,CHS_t* CHS);
+void console_turnToCHS(uint32_t* sectorNum,t_CHS* CHS);
 
-void console_showTrace(char* msg);
+//void console_showTrace(char* msg);
 
 #endif /* PPDCONSOLE_COMMAND_H_ */
