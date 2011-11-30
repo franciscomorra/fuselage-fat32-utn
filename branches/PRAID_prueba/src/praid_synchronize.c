2819aac52dee22c4e02a56b9631bd6708a96e6bc
santiago.carullo@gmail.com <santiago.carullo@gmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-11-30 21:15:02 +0000

    - Prueba con PFS y 1 Disco OK

--- branches/PRAID_prueba/src/praid_synchronize.c
+++ branches/PRAID_prueba/src/praid_synchronize.c
@@ -9,23 +9,23 @@
 #include "praid_ppdlist.h"
 #include <stdint.h>
 
-extern uint32_t total_sectors;
-extern uint32_t sync_write_count;
+extern uint32_t sectors_perDisk;
+extern uint32_t pending_writes_forSyncronization;
 
 void *synchronize_thread(void *ppd_info)
 {
 	ppd_node_t *ppd_node = (ppd_node_t*)ppd_info;
-	sync_write_count = total_sectors;
+	pending_writes_forSyncronization = sectors_perDisk;
 
 	uint32_t sector = 0;
-	for (;sector < total_sectors;sector++)
+	for (;sector < sectors_perDisk;sector++)
 	{
 		char *sync_msg = malloc(11);
 		*sync_msg = 0x01;
 		*((uint16_t*) (sync_msg+1)) = 8;
 		*((uint32_t*) (sync_msg+3)) = 0;
 		*((uint32_t*) (sync_msg+7)) = sector;
-		PFSREQUEST_addNew(ppd_node->ppd_fd,sync_msg);
+		pfs_request_addNew(ppd_node->ppd_fd,sync_msg);
 		free(sync_msg);
 	}
 
