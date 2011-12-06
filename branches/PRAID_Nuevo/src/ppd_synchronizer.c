3c95b10166719b46a912b4cee2874797b977f18f
santiago.carullo@gmail.com <santiago.carullo@gmail.com@1d167dc6-e0c8-2c6e-d573-7fd531939ff2>
2011-12-06 17:59:52 +0000

    - CheckSocket agregado, se fija si se puede escribir en el socket, , sincro ok

--- branches/PRAID_Nuevo/src/ppd_synchronizer.c
+++ branches/PRAID_Nuevo/src/ppd_synchronizer.c
@@ -8,44 +8,44 @@
 
 
 #include <sys/ioctl.h>
-#include <sockets.h>
+#include <sys/socket.h>
 #include "nipc.h"
 #include "ppd_synchronizer.h"
 #include "ppd_queue.h"
 #include "tad_sockets.h"
 
-extern uint32_t one_sector_read_time;
-extern uint32_t one_sector_write_time;
+uint32_t checkSocketSend(uint32_t fd);
 
 void* ppd_synchronizer(void *data)
 {
 	ppd_node_t *ppd_info = (ppd_node_t*) data;
 	uint32_t sectors_to_synchronize = ppd_info->sectors_count;
+
 	uint32_t requests_sent = 0, requests_received = 0;
 	char *msg_buf;
 
 	while (requests_received < sectors_to_synchronize)
 	{
-
-
-
-
-		if (requests_sent < sectors_to_synchronize)
+		if (requests_sent < sectors_to_synchronize )
 		{
 			ppd_node_t *selected_ppd = PPDQUEUE_selectByLessRequests();
 
+			if (checkSocketSend(selected_ppd->ppd_fd))
+			{
 			//pthread_mutex_lock(&selected_ppd->sock_mutex);
-			msg_buf = malloc(11);
-			*msg_buf = READ_SECTORS;
-			*((uint16_t*)(msg_buf+1)) = 8;
-			*((uint32_t*)(msg_buf+3)) = 0;
-			*((uint32_t*)(msg_buf+7)) = requests_sent;
-			//one_sector_read_time = getMicroseconds();
-			send(selected_ppd->ppd_fd,msg_buf,11,MSG_WAITALL);
-			//SOCKET_sendAll(selected_ppd->ppd_fd,msg_buf,11,0);
-			//send(selected_ppd->ppd_fd,msg_buf,11,MSG_WAITALL);
-			requests_sent++;
-			free(msg_buf);
+				msg_buf = malloc(11);
+				*msg_buf = READ_SECTORS;
+				*((uint16_t*)(msg_buf+1)) = 8;
+				*((uint32_t*)(msg_buf+3)) = 0;
+				*((uint32_t*)(msg_buf+7)) = requests_sent;
+				//one_sector_read_time = getMicroseconds();
+				//send(selected_ppd->ppd_fd,msg_buf,11,MSG_WAITALL);
+
+				SOCKET_sendAll(selected_ppd->ppd_fd,msg_buf,11,0);
+				//send(selected_ppd->ppd_fd,msg_buf,11,MSG_WAITALL);
+				requests_sent++;
+				free(msg_buf);
+			}
 			//pthread_mutex_unlock(&selected_ppd->sock_mutex);
 		}
 
@@ -65,4 +65,15 @@ void* ppd_synchronizer(void *data)
 	}
 
 	return 1;
+}
+
+uint32_t checkSocketSend(uint32_t fd)
+{
+	fd_set write_set;
+	FD_ZERO(&write_set);
+	FD_SET(fd,&write_set);
+	uint32_t breakp;
+
+	return select(fd+1,NULL,&write_set,NULL,0) == 0 ? 0 : 1;
+
 }
